51482f0786471fda768817e621e1d7f152bf09e3
unknown <lehestiklennar@gmail.com>
2020-07-03 19:46:44 +0300

    ''

--- App.js
+++ App.js
@@ -1,14 +1,50 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useEffect, useState} from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+import AnchorLink from 'anchor-link'
+import AnchorLinkBrowserTransport from 'anchor-link-browser-transport'
+
+const transport = new AnchorLinkBrowserTransport()
+// initialize the link, this time we are using the TELOS chain
+const link = new AnchorLink({transport,
+    chainId: '5fff1dae8dc8e2fc4d5b23b2c7665c97f9e9d8edf2b6485a86ba311c25639191',
+    rpc: 'https://kylin-dsp-2.liquidapps.io'
+})
+// the EOSIO action we want to sign and broadcast
+const action = {
+    account: 'andrtestcons',
+    name: 'createpollz',
+    authorization: [{
+        actor: '............1', // ............1 will be resolved to the signing accounts permission
+        permission: '............2' // ............2 will be resolved to the signing accounts authority
+    }],
+    data: {
+      question: "Question?",
+      answers: ["First", "Second", "Third", "Last"],
+      totalvote: [0,0,0,100],
+      community: "eyaltestcons",
+      creator: "andrtestcons"
+    }
+}
+// ask the user to sign the transaction and then broadcast to chain
+function vote() {
+    link.transact({action})
+        .then((result) => {
+            document.body.innerHTML = `<h1>Thank you ${ result.signer.actor }!</h1>`
+        })
+}
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <Button
+      onPress={vote()}
+      title="Vote"
+      color="#841584"
+      />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
@@ -17,5 +53,5 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-  },
+  }
 });
